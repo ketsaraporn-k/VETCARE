944a15c55ce2369319d244c644d1f2ab7dbe19ad
Tatsunori-Kato-Spu <tatsunori.kat@spumai.net>
2025-11-01 13:24:08 +0700

    Add_middleware

---
+++ backEnd/middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next) {
+  const token = req.headers['authorization'];
+
+  if (!token) return res.status(401).json({ error: 'No token provided' });
+
+  try {
+    const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
