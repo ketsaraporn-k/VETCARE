49cb772e7469e84859a626d0007ce28f0b5a44c1
ketsaraporn k. <ketsaraporn.yai@spumail.net>
2025-11-02 23:53:52 +0700

    edit import

--- backEnd/routes/userRoutes.js
+++ backEnd/routes/userRoutes.js
@@ -1,7 +1,7 @@
 // routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/{king}-userController');
+const userController = require('../controllers/userController');
 const auth = require('../middleware/auth');
 const role = require('../middleware/role');
 
